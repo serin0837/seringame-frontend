439ec94e2c41a915f6fd10589afc8787f1f85bf6
Serin Jeon <serin0837@yahoo.com>
2020-06-23 16:13:35 +0100

    start front end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import Title from "./Component/Title";
+import Traveller from "./Component/Traveller";
+import Countries from "./Component/Countries";
+import { Router } from "@reach/router";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Title></Title>
+      <Traveller></Traveller>
+      <Router>
+        <Countries path="/"></Countries>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Component/Countries.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Countries extends Component {
+  render() {
+    return (
+      <main>
+        <p>countries list</p>
+      </main>
+    );
+  }
+}
+
+export default Countries;
