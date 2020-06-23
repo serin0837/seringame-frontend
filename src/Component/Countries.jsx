5a6b1c3dba274aec1074f9e7815a3e2ce41bc147
Serin Jeon <serin0837@yahoo.com>
2020-06-23 16:54:47 +0100

    can not load country yet

--- src/Component/Countries.jsx
+++ src/Component/Countries.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Countries extends Component {
+  state = {
+    countries: [],
+    isLoading: true,
+  };
+  componentDidMount() {
+    axios.get("https://seringame.herokuapp.com/countries").then(({ data }) => {
+      console.log({ data });
+      //   this.setState({ countries: data, isLoading: false });
+    });
+  }
   render() {
+    const { countries, isLoading } = this.state;
     return (
       <main>
-        <p>countries list</p>
+        <p>{countries.country_name}</p>
+        <p>country come out</p>
       </main>
     );
   }
